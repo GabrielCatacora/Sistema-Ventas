7c4bb3ce7e4bcdcc3021db6768a18905109f09ec
GabrielCatacora <gcatacorasilva@gmail.com>
2023-01-24 00:45:59 -0500

    Container login

--- Sistema-Ventas/src/app/app.component.ts
+++ Sistema-Ventas/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation,} from '@angular/core';
+//import { LoginComponent } from './pages/shared/login/login/login.component';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+
   title = 'Sistema-Ventas';
+
 }
